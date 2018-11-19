8a9c076afa691068cf0d25b5c762531cf95838bf
SHO <160756@jc-21.jp>
2018-11-19 12:03:11 +0900

    課題ができた

---
+++ BMI/BMI/BmiResult.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BMI
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BmiResult : ContentPage
+	{
+        private float BMI;
+
+        public BmiResult()
+        {
+            InitializeComponent();
+            buttonBack.Clicked += buttonBackClicked;
+        }
+
+        public BmiResult(float BMI)
+        {
+            InitializeComponent();
+            this.BMI = BMI;
+            labelBMI.Text = "BMI=" + BMI;
+            buttonBack.Clicked += buttonBackClicked;
+        }
+        
+        
+        private void buttonBackClicked(object sender, EventArgs e)
+        {
+            Navigation.PopModalAsync();
+        }
+    }
+}
--- BMI/BMI/MainPage.xaml.cs
+++ BMI/BMI/MainPage.xaml.cs
@@ -12,6 +12,48 @@ namespace BMI
         public MainPage()
         {
             InitializeComponent();
+            ButtonBMI.Clicked += ButtonBmiEvent;
+            ButtonBMI2.Clicked += ButtonClickEvent2;
+        }
+
+        private void ButtonClickEvent2(object sender, EventArgs e)
+        {
+            float height = float.Parse(EntryHeight.Text);
+            float weight = float.Parse(Entry.Text);
+
+            if (height <= 3)
+            {
+                LabelResult.Text = "" + weight / (height * height);
+                float BMI = weight / (height * height);
+                Navigation.PushModalAsync(new NavigationPage(new BmiResult(BMI)));
+            }
+            else
+            {
+                height = height / 100;
+                LabelResult.Text = "" + weight / (height * height);
+                float BMI = weight / (height * height);
+                Navigation.PushModalAsync(new NavigationPage(new BmiResult(BMI)));
+            }
+
+
+            
+            
+        }
+        private void ButtonBmiEvent(object sender, EventArgs e)
+        {
+
+            float height = float.Parse(EntryHeight.Text);
+            float weight = float.Parse(Entry.Text);
+
+            if (height <= 3)
+            {
+               LabelResult.Text = "" + weight / (height * height);
+            }
+            else
+            {
+                height = height / 100;
+                LabelResult.Text = "" + weight / (height * height);
+            }
         }
     }
 }
